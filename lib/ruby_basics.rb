4c41526f4403740f825e64a936560cc2771dfb66
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-22 04:01:47 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,16 +1,16 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(arg)
+  return arg
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
